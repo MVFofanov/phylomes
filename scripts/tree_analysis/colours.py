332450682ee6daca3a381d3bf8f53be64a532447
MVFofanov <MVFofanov@mail.ru>
2024-08-20 17:47:16 +0200

    feat: add colour names and fix a bug with Actinomycetota colour

--- scripts/tree_analysis/colours.py
+++ scripts/tree_analysis/colours.py
@@ -1,28 +1,28 @@
 from typing import Dict
 
 source_colors: Dict[str, str] = {
-    'ncbi': '#1f78b4',
-    'phylome': '#e31a1c'
+    'ncbi': '#1f78b4',  # Steel Blue
+    'phylome': '#e31a1c'  # Red
 }
 
 superkingdom_colors: Dict[str, str] = {
-    'Bacteria': '#33a02c',
-    'Viruses': '#6a3d9a',
+    'Bacteria': '#33a02c',  # Forest Green
+    'Viruses': '#6a3d9a',  # Royal Purple
     'Other': 'gray'  # Default color for any other superkingdoms
 }
 
 phylum_colors: Dict[str, str] = {
-    'Actinobacteria': '#b2df8a',
-    'Actinomycetota': '##b2df8a',
-    'Bacillota': '#a6cee3',
-    'Bacteroidetes': '#ff7f00',
-    'Bacteroidota': '#ff7f00',
-    'Cyanobacteriota': '#ffff99',
-    'Firmicutes': '#a6cee3',
-    'Proteobacteria': '#b15928',
-    'Pseudomonadota': '#b15928',
-    'Uroviricota': '#cab2d6',
-    'Other': 'gray'
+    'Actinobacteria': '#b2df8a',  # Light Green
+    'Actinomycetota': '#b2df8a',  # Light Green (same as Actinobacteria)
+    'Bacillota': '#a6cee3',  # Light Blue
+    'Bacteroidetes': '#ff7f00',  # Orange
+    'Bacteroidota': '#ff7f00',  # Orange (same as Bacteroidetes)
+    'Cyanobacteriota': '#ffff99',  # Light Yellow
+    'Firmicutes': '#a6cee3',  # Light Blue (same as Bacillota)
+    'Proteobacteria': '#b15928',  # Brown
+    'Pseudomonadota': '#b15928',  # Brown (same as Proteobacteria)
+    'Uroviricota': '#cab2d6',  # Light Purple
+    'Other': 'gray'  # Default color for any other phyla
 }
 
-crassvirales_color = '#fb9a99'
+crassvirales_color = '#fb9a99'  # Light Pink
